525862ffda181e3361f74a54d9ac75834d173302
Damiano <damianoficara@gmail.com>
2022-06-13 17:26:55 +0200

    Creazione menu

--- app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
@@ -2,11 +2,40 @@ package it.com.uninsubria.footballteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import it.com.uninsubria.footballteam.fragments.AtletiFragment
+import it.com.uninsubria.footballteam.fragments.ChatFragment
+import it.com.uninsubria.footballteam.fragments.HomeFragment
+import it.com.uninsubria.footballteam.fragments.LockFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // creazione di un nuovo branch
+
+
+        val home = HomeFragment()
+        val atleti = AtletiFragment()
+        val lock = LockFragment()
+        val chat = ChatFragment()
+
+        // Creazione Home
+        creazioneFragment(home)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> creazioneFragment(home)
+                R.id.atleti -> creazioneFragment(atleti)
+                R.id.lock -> creazioneFragment(lock)
+                R.id.chat -> creazioneFragment(chat)
+            }
+            true
+        }
     }
+
+    private fun creazioneFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.contenitore, fragment)
+            commit()
+        }
 }
