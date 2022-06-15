6517e632bd8d94554dddf7d8d7814b47eb6fe6d2
alessandroCassani <Lerobinie!8>
2022-06-15 11:41:46 +0200

    aggiunta listener ai buttons per switch fragment

--- app/src/main/java/it/com/uninsubria/footballteam/Login.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/Login.kt
@@ -2,11 +2,26 @@ package it.com.uninsubria.footballteam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login.*
 
 class Login : AppCompatActivity() {
+    private val signIn = SignIn()
+    private val signUp = SignUp()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        btn_SignUp.setOnClickListener{
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView,signUp)
+            transaction.commit()
+        }
+
+        btn_SignIn.setOnClickListener{
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView,signIn)
+            transaction.commit()
+        }
     }
 }
