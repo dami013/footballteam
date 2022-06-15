861f79b168f1e172a8172bbade2abb29f793ffd4
Damiano <damianoficara@gmail.com>
2022-06-15 22:31:02 +0200

    Modifica struttura progetto

--- app/src/main/java/it/com/uninsubria/footballteam/Login.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/Login.kt
@@ -2,29 +2,16 @@ package it.com.uninsubria.footballteam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.fragment_sign_in.*
-import kotlinx.android.synthetic.main.login.*
-import kotlinx.android.synthetic.main.login.btn_SignIn
+
+
 
 class Login : AppCompatActivity() {
-    private val signIn = SignIn()
-    private val signUp = SignUp()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        btn_SignUp.setOnClickListener{
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainerView,signUp)
-            transaction.commit()
-        }
 
-        btn_SignIn.setOnClickListener{
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainerView,signIn)
-            transaction.commit()
-        }
 
 
 
