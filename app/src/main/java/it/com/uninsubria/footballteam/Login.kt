01b71a435aa667a79d02dd4dcc2db770219ac316
Damiano <damianoficara@gmail.com>
2022-06-15 22:51:42 +0200

    Creazione Signup

--- app/src/main/java/it/com/uninsubria/footballteam/Login.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/Login.kt
@@ -1,8 +1,9 @@
 package it.com.uninsubria.footballteam
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.login.*
 
 
 class Login : AppCompatActivity() {
@@ -11,6 +12,10 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        signUp.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
--- app/src/main/java/it/com/uninsubria/footballteam/RegisterActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/RegisterActivity.kt
@@ -2,15 +2,14 @@ package it.com.uninsubria.footballteam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.login.*
 
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
-
+        setContentView(R.layout.register)
 
 
 
