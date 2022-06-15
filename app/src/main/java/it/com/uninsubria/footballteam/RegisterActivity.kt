7588d78d1297795a0e3d139dce0785cf8099b081
Damiano <damianoficara@gmail.com>
2022-06-15 22:31:17 +0200

    Modifica struttura progetto

---
+++ app/src/main/java/it/com/uninsubria/footballteam/RegisterActivity.kt
@@ -0,0 +1,20 @@
+package it.com.uninsubria.footballteam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+
+
+
+
+    }
+
+}
