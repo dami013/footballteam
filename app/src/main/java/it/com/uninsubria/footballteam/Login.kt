5cb38ab5a5d0a2b80548b6e5e0b748cabed30231
alessandroCassani <Lerobinie!8>
2022-06-16 11:04:51 +0200

    aggiunta textView login in LoginActivity, senza guideline. fissato bug di visualizzazione button register

--- app/src/main/java/it/com/uninsubria/footballteam/Login.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.register.*
 
 
 class Login : AppCompatActivity() {
@@ -11,6 +12,7 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
 
         signUp.setOnClickListener {
             val intent = Intent(this,RegisterActivity::class.java)
--- app/src/main/java/it/com/uninsubria/footballteam/RegisterActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/RegisterActivity.kt
@@ -1,8 +1,10 @@
 package it.com.uninsubria.footballteam
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.register.*
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,8 +13,10 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
 
-
-
+        login.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
 
     }
 
