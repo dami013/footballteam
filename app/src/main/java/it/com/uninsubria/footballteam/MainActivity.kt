2f48f17271bbcd875e392771d255adb23ffab92d
Damiano <damianoficara@gmail.com>
2022-06-17 19:08:47 +0200

    Predispozione adapter e viewholder

--- app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/it/com/uninsubria/footballteam/adapter/PlayerViewHolder.kt
@@ -0,0 +1,4 @@
+package it.com.uninsubria.footballteam.adapter
+
+class PlayerViewHolder {
+}
