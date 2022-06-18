bd2ced7c9dde43a6159850c52f291b92c01fa052
alessandroCassani <Lerobinie!8>
2022-06-18 17:31:41 +0200

    aggiunto drawable per image view e modifica colore

--- app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import it.com.uninsubria.footballteam.adapter.PlayerAdapter
-import it.com.uninsubria.footballteam.fragments.AtletiFragment
-import it.com.uninsubria.footballteam.fragments.ChatFragment
-import it.com.uninsubria.footballteam.fragments.HomeFragment
-import it.com.uninsubria.footballteam.fragments.LockFragment
+import it.com.uninsubria.footballteam.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,26 +20,21 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,"Benvenuto",Toast.LENGTH_SHORT).show()
 
-        val home = HomeFragment()
         val atleti = AtletiFragment()
-        val lock = LockFragment()
+        val nuovo = register_player_fragment()
         val chat = ChatFragment()
 
         // Creazione Home
-        creazioneFragment(home)
         // Modifica metodo precedentemente deprecato
+        creazioneFragment(atleti)
         bottom_navigation.setOnItemSelectedListener  {
             when(it.itemId) {
-                R.id.homeFrag -> creazioneFragment(home)
                 R.id.atleti -> creazioneFragment(atleti)
-                R.id.lock -> creazioneFragment(lock)
+                R.id.nuovo -> creazioneFragment(nuovo)
                 R.id.chat -> creazioneFragment(chat)
             }
             true
         }
-
-
-
     }
 
     private fun creazioneFragment(fragment: Fragment) =
--- app/src/main/java/it/com/uninsubria/footballteam/fragments/AtletiFragment.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/fragments/AtletiFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,8 +14,6 @@ import it.com.uninsubria.footballteam.R
 import it.com.uninsubria.footballteam.adapter.PlayerAdapter
 import kotlinx.android.synthetic.main.fragment_atleti.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -25,9 +24,10 @@ private const val ARG_PARAM2 = "param2"
  */
 
 class AtletiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+    private val fragmentAtl = register_player_fragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +35,15 @@ class AtletiFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_atleti, container, false)
         startRecyclerView(view)
         return view
-
-
     }
+
     private fun startRecyclerView(view: View) {
         val recycler = view.findViewById<RecyclerView>(R.id.recycler_view)
         val manager = LinearLayoutManager(view.context)
@@ -54,10 +51,7 @@ class AtletiFragment : Fragment() {
         recycler.layoutManager = manager
         recycler.adapter = PlayerAdapter(Dataset.giocatori)
         recycler.addItemDecoration(decorazione)
-
     }
-
-
 
     companion object {
         /**
