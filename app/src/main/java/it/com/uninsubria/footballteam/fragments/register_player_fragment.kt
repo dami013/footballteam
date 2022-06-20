7c26cfb95953bcb4db3cfcbfaecd0ef24c9fcf5e
alessandroCassani <Lerobinie!8>
2022-06-20 17:59:07 +0200

    fixed bug in register player

--- app/src/main/java/it/com/uninsubria/footballteam/fragments/register_player_fragment.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/fragments/register_player_fragment.kt
@@ -3,18 +3,28 @@ package it.com.uninsubria.footballteam.fragments
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import it.com.uninsubria.footballteam.MainActivity
 import it.com.uninsubria.footballteam.R
 import kotlinx.android.synthetic.main.register.*
 import kotlinx.android.synthetic.main.register_player_fragment.*
 import kotlinx.android.synthetic.main.register_player_fragment.view.*
 import java.io.ByteArrayOutputStream
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -24,14 +34,32 @@ class register_player_fragment : Fragment(){
     private var param1: String? = null
     private var param2: String? = null
     private val FOTO = 1
-    private val FireBaseRef = FirebaseDatabase.getInstance("https://footballteam-d5795-default-rtdb.firebaseio.com/").getReference("atleta")
+    private val ref = FirebaseDatabase.getInstance("https://footballteam-d5795-default-rtdb.firebaseio.com/").getReference("Atleti")
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+    private fun drawableToBitmap(drawable: Drawable): Bitmap? {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        var width = drawable.intrinsicWidth
+        width = if (width > 0) width else 1
+        var height = drawable.intrinsicHeight
+        height = if (height > 0) height else 1
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+        drawable.draw(canvas)
+        return bitmap
     }
 
     override fun onCreateView(
@@ -40,6 +68,8 @@ class register_player_fragment : Fragment(){
     ): View? {
         // Inflate the layout for this fragment
         val view: View =  inflater.inflate(R.layout.register_player_fragment, container, false)
+        auth = Firebase.auth
+         val main = AtletiFragment()
 
         view.immagine.setOnClickListener{
             openGalleryForImage()
@@ -47,6 +77,16 @@ class register_player_fragment : Fragment(){
 
         view.register.setOnClickListener{
             onRegisterClick()
+            view.register.setOnClickListener(View.OnClickListener {
+                val fragmentManager = parentFragmentManager
+                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(
+                    R.id.mainContainer,
+                    main
+                )
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+            })
         }
 
         return view
@@ -57,8 +97,8 @@ class register_player_fragment : Fragment(){
         val name = nome.text.toString().trim()
         val codFisc = cf.text.toString().trim()
         val cogn = cognome.text.toString().trim()
-        (immagine.drawable as BitmapDrawable).bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytearros)
-        val data = bytearros.toByteArray()
+        drawableToBitmap(immagine.drawable)?.compress(Bitmap.CompressFormat.JPEG, 100, bytearros)
+        val data = String (bytearros.toByteArray())
         val dataN = dataNascita.text.toString().trim()
         val cel = phone.text.toString().trim()
         val cert = certificazione?.text.toString().trim()  //certificazioni e risultati possono essere nulli
@@ -77,7 +117,7 @@ class register_player_fragment : Fragment(){
             return
         }
         if(cel.isEmpty()){
-            phone.error = "inserire numerop di telefono"
+            phone.error = "inserire numero di telefono"
             return
         }
 
@@ -92,9 +132,21 @@ class register_player_fragment : Fragment(){
         cel: String,
         cert: String,
         ris: String,
-        data: ByteArray
+        data: String
     ) {
 
+        val currentUser = auth.currentUser
+        val uid = currentUser!!.uid
+        val atletiMap = HashMap<String, String>()
+        atletiMap["nome"] = name
+        atletiMap["cognome"] = cogn
+        atletiMap["dataNascita"] = dataN
+        atletiMap["codiceFiscale"] = codFisc
+        atletiMap["telefono"] = cel
+        atletiMap["certificazioni"] = cert
+        atletiMap["risultati"] = ris
+        atletiMap["immagine"] = data
+        ref.child(uid).setValue(atletiMap)
     }
 
 
