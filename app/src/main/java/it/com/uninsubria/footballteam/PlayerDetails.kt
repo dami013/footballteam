32e0211a7edd3be4fe8fe84d5f6de8bbaca6a054
alessandroCassani <Lerobinie!8>
2022-06-22 17:28:42 +0200

    aggiunto metodo inserzione dati per visualizzazione specifica

--- app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
@@ -3,10 +3,12 @@ package it.com.uninsubria.footballteam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import it.com.uninsubria.footballteam.adapter.PlayerAdapter
+import kotlinx.android.synthetic.main.activity_player_details.*
 
 class PlayerDetails : AppCompatActivity() {
     private  lateinit var db: DatabaseReference
@@ -17,5 +19,34 @@ class PlayerDetails : AppCompatActivity() {
 
     }
 
+    private fun insertData(cf: String){
+        val auth = Firebase.auth
+        val currentUser = auth.currentUser
+        val uid = currentUser!!.uid
+        db = FirebaseDatabase.getInstance("https://footballteam-d5795-default-rtdb.firebaseio.com/")
+            .getReference("Users")
+            .child(uid)
+            .child("Atleti")
+            .child(cf)
+        db.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    val atleta = snapshot.getValue(Atleta::class.java)
+                    //manca foto
+                    name.text = atleta!!.nome
+                    surname.text = atleta.cognome
+                    codFisc.text = atleta.codiceFiscale
+                    dataN.text = atleta.dataNascita
+                    ruolo.text = atleta.ruolo
+                    phone.text = atleta.phone
+                    results.text = atleta.risultati
+                    certificazioni.text = atleta.certificazione
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TEST","Failed to read value")
+            }
+        })
+    }
 }
