a68f4e7f070cc9991db7988dcd24d252dcaac2c1
alessandroCassani <Lerobinie!8>
2022-06-24 16:48:17 +0200

    aggiunto fragmentContainer e crezione layout dei 2 fragment principali

--- app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
@@ -1,53 +1,21 @@
 package it.com.uninsubria.footballteam
 
-
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_player_details.*
+
 
 class PlayerDetails : AppCompatActivity() {
     private  lateinit var db: DatabaseReference
+    private val fragmentDetails = viusalize_players_details()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_details)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,fragmentDetails).commit()
         val cf = intent.getStringExtra("cf")
-        insertData(cf!!)
+       // insertData(cf!!)
     }
 
-    private fun insertData(cf: String){
-        val auth = Firebase.auth
-        val currentUser = auth.currentUser
-        val uid = currentUser!!.uid
-        db = FirebaseDatabase.getInstance("https://footballteam-d5795-default-rtdb.firebaseio.com/")
-            .getReference("Users")
-            .child(uid)
-            .child("Atleti")
-            .child(cf)
-        db.addValueEventListener(object : ValueEventListener {
-            @SuppressLint("SetTextI18n")
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()) {
-                    val atleta = snapshot.getValue(Atleta::class.java)
-                    //manca foto
-                    name.setText("nome ${atleta!!.nome}")
-                    surname.setText("cognome ${atleta.cognome}")
-                    codFisc.setText("codice fiscale : ${atleta.codiceFiscale}")
-                    dataN.setText(" data di nascita  + ${atleta.dataNascita}")
-                    ruolo.setText("ruolo + ${atleta.ruolo}")
-                    phone.setText("telefono ${atleta.phone}")
-                    results.setText("risultati ${atleta.risultati}")
-                    certificazioni.setText("certificazioni ${atleta.certificazione}")
-                }
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("TEST","Failed to read value")
-            }
-        })
-    }
 }
