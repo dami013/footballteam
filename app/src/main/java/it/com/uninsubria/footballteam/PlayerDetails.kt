33378cab93a159092e2f5b428730f9f3bc8f06a6
alessandroCassani <Lerobinie!8>
2022-06-24 17:00:55 +0200

    aggiunto listener su modifica giocatore

--- app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
@@ -1,19 +1,30 @@
 package it.com.uninsubria.footballteam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.*
+import it.com.uninsubria.footballteam.fragments.Change_player_details
+import kotlinx.android.synthetic.main.fragment_visualize_players_details.*
 
 
 class PlayerDetails : AppCompatActivity() {
     private  lateinit var db: DatabaseReference
     private val fragmentDetails = viusalize_players_details()
+    private val changeDetails = Change_player_details()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_details)
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainerView,fragmentDetails).commit()
         val cf = intent.getStringExtra("cf")
+
+        modifica.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, changeDetails)
+                commit()
+            }
+        }
        // insertData(cf!!)
     }
 
