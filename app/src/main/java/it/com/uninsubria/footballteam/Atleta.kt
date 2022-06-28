e472746afab755d3678ac01dbe470adf18a6c738
alessandroCassani <Lerobinie!8>
2022-06-28 18:12:53 +0200

    utilizzo bundle per passaggio dqati e modifica metodo con setText

--- app/src/main/java/it/com/uninsubria/footballteam/Atleta.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/Atleta.kt
@@ -1,15 +1,15 @@
 package it.com.uninsubria.footballteam
 
 data class Atleta(
-    var nome: String?,
-    var cognome: String?,
-    var dataNascita: String?,
-    var codiceFiscale: String?,
-    var ruolo: String?,
-    var phone: String?,
-    var certificazione: String?,
-    var photo : String?,
-    var risultati: String?,
+    var nome: String,
+    var cognome: String,
+    var dataNascita: String,
+    var codiceFiscale: String,
+    var ruolo: String,
+    var phone: String,
+    var certificazione: String,
+    var photo : String,
+    var risultati: String,
     ) {
     // Serve un costruttore vuoto per firebase
 
@@ -18,20 +18,8 @@ data class Atleta(
     // ci interssa di porre nome e cognome
     override fun toString(): String {
         return nome.toString()
-        return cognome.toString()
-        return ruolo.toString()
+        return cognome
+        return ruolo
     }
     // Permette di sovrascrivere i valori precedente con i nuovi
-    fun set(atleta: Atleta) {
-        nome = atleta.nome
-        cognome = atleta.cognome
-        dataNascita = atleta.dataNascita
-        codiceFiscale = atleta.codiceFiscale
-        ruolo = atleta.ruolo
-        phone = atleta.phone
-        certificazione = atleta.certificazione
-        photo = atleta.photo
-        risultati = atleta.risultati
-
-    }
 }
