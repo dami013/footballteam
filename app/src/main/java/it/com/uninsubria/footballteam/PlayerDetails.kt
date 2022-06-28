96d2e9cbe94872fe1fcbe868bfbcdd93c3d0cc14
alessandroCassani <Lerobinie!8>
2022-06-28 16:07:26 +0200

    fissato bug switch tra fragment con ovverride onViewCreated

--- app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/PlayerDetails.kt
@@ -1,8 +1,7 @@
 package it.com.uninsubria.footballteam
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import it.com.uninsubria.footballteam.fragments.Change_player_details
 import kotlinx.android.synthetic.main.fragment_visualize_players_details.*
@@ -12,21 +11,19 @@ class PlayerDetails : AppCompatActivity() {
     private  lateinit var db: DatabaseReference
     private val fragmentDetails = viusalize_players_details()
     private val changeDetails = Change_player_details()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_details)
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmentContainerView,fragmentDetails).commit()
         val cf = intent.getStringExtra("cf")
 
-        modifica.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView, changeDetails)
-                commit()
-            }
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer,fragmentDetails).commit()
+
+
+        //passaggio dati da activity a fragment
+        val args = Bundle()
+        args.putString("cf", cf)
+        fragmentDetails.setArguments(args)
         }
-       // insertData(cf!!)
     }
-
-
-}
--- app/src/main/java/it/com/uninsubria/footballteam/visualize_players_details.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/visualize_players_details.kt
@@ -1,18 +1,27 @@
 package it.com.uninsubria.footballteam
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import it.com.uninsubria.footballteam.fragments.Change_player_details
+import kotlinx.android.synthetic.main.activity_welcome.*
+import kotlinx.android.synthetic.main.fragment_visualize_players_details.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class viusalize_players_details : Fragment() {
-    // TODO: Rename and change types of parameters
+    private  lateinit var db: DatabaseReference
+    private val changePlayers = Change_player_details()
     private var param1: String? = null
     private var param2: String? = null
 
@@ -22,13 +31,65 @@ class viusalize_players_details : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mod.setOnClickListener(View.OnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(
+                R.id.fragmentContainer,
+                changePlayers
+            )
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        })
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_visualize_players_details,container, false)
+        val view = inflater.inflate(R.layout.fragment_visualize_players_details,container, false)
+
+        val args = Bundle()
+        val cf = args.getString("cf", "")
+        insertData(cf!!)
+        return view
+    }
+
+    private fun insertData(cf: String){
+        val auth = Firebase.auth
+        val currentUser = auth.currentUser
+        val uid = currentUser!!.uid
+        db = FirebaseDatabase.getInstance("https://footballteam-d5795-default-rtdb.firebaseio.com/")
+            .getReference("Users")
+            .child(uid)
+            .child("Atleti")
+            .child(cf)
+        db.addValueEventListener(object : ValueEventListener {
+            @SuppressLint("SetTextI18n")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    val atleta = snapshot.getValue(Atleta::class.java)
+                    //manca foto
+                    nome.text = "nome: ${atleta!!.nome}"
+                    cognome.text = "cognome: ${atleta.cognome}"
+                    codFisc.text = "codice fiscale : ${atleta.codiceFiscale}"
+                    dataN.text = " data di nascita  + ${atleta.dataNascita}"
+                    ruolo.text = "ruolo + ${atleta.ruolo}"
+                    telefono.text = "telefono ${atleta.phone}"
+                    risultati.text = "risultati ${atleta.risultati}"
+                    certificati.text = "certificazioni ${atleta.certificazione}"
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TEST","Failed to read value")
+            }
+        })
     }
 }
