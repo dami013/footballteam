12f07fce671d58ebd7c11f7f1300be4f2f176ecb
Damiano <damianoficara@gmail.com>
2022-07-05 12:35:24 +0200

    Definzione toolbar

--- app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/MainActivity.kt
@@ -4,19 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import it.com.uninsubria.footballteam.adapter.PlayerAdapter
 import it.com.uninsubria.footballteam.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Communicator {
 
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Football team"
+        setSupportActionBar(toolbar)
 
         Toast.makeText(this,"Benvenuto",Toast.LENGTH_SHORT).show()
 
