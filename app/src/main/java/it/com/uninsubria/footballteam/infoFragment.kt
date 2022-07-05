b2a425634a6361a7b9fdedcf067b84d6a250e089
alessandroCassani <Lerobinie!8>
2022-07-05 17:18:42 +0200

    sign out diretto su login con aggiunta flag

--- app/src/main/java/it/com/uninsubria/footballteam/infoFragment.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/infoFragment.kt
@@ -1,6 +1,7 @@
 package it.com.uninsubria.footballteam
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -48,6 +49,10 @@ class infoFragment : Fragment() {
 
         btn_signOut.setOnClickListener{
             auth.signOut()
+            var intent = Intent(this.requireContext(), Login::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
             Toast.makeText(this.requireContext(), "sign out", Toast.LENGTH_SHORT).show()
         }
     }
