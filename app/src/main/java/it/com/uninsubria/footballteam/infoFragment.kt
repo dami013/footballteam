c89f177e8407cf891f01079a25a8c03d4babe0e2
Damiano <damianoficara@gmail.com>
2022-07-06 16:18:57 +0200

    Sistemazione bug info fragment

--- app/src/main/java/it/com/uninsubria/footballteam/infoFragment.kt
+++ app/src/main/java/it/com/uninsubria/footballteam/infoFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
@@ -21,39 +22,39 @@ private const val ARG_PARAM2 = "param2"
 
 
 class infoFragment : Fragment() {
-    private var param1: String? = null
+
     private  lateinit var db: DatabaseReference
     private var auth = Firebase.auth
-    private var param2: String? = null
+    private lateinit var username: TextView
+    private lateinit var email: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
 
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        insertData()
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        // Inflate the layout for this fragmen
+        val view =  inflater.inflate(R.layout.fragment_info, container, false)
+        username = view.findViewById(R.id.username)
+        email = view.findViewById(R.id.email)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        insertData()
         btn_signOut.setOnClickListener{
             auth.signOut()
             var intent = Intent(this.requireContext(), Login::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
-            Toast.makeText(this.requireContext(), "sign out", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.requireContext(), "Sign Out", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -65,7 +66,6 @@ class infoFragment : Fragment() {
             .child(uid)
 
         db.addValueEventListener(object : ValueEventListener {
-            @SuppressLint("SetTextI18n")
             override fun onDataChange(snapshot: DataSnapshot) {
                 if(snapshot.exists()) {
                     val user = snapshot.getValue(User::class.java)!!
